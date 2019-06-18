f5789b4a149a55b46495a6c8045fcf7dbfb32f3c
Shadowborn <james.starks023@gmail.com>
2019-06-17 23:21:43 -0600

    fial tests working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,7 +13,18 @@ function succeed(item) {
   return { ...item, enhancement };
 }
 
-// enhancer repair should make durabiity 100
+function fail(item) {
+  let enhancement = item.enhancement;
+  let durability = item.durability;
+  if (enhancement < 10){
+    durability = durability - 4;
+  } else {
+    durability = durability - 6;
+  }
+  return { ...item, enhancement, durability };
+}
+
+// enhancer repair should make durability 100
 function repair(item) {
   item = { ...item, durability: 100 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,8 @@
-const enhancer = require('./enhancer.js');
+// const enhancer = require('./enhancer.js');
 // test away!
 const {repair} = require('./enhancer.js');
 const {succeed} = require('./enhancer.js');
+const {fail} = require('./enhancer.js');
 
 const weapon = {
     name: "Poison Dagger",
@@ -11,6 +12,7 @@ const weapon = {
 
 describe('enhancer.js', () => {
 
+    // REPAIR TEST
     describe('repair()', () => {
         it('restores durability to 100', () => {
             expect(repair({ durability: 89 }).durability).toBe(100);
@@ -20,13 +22,20 @@ describe('enhancer.js', () => {
         });
     });
 
-    // it.todo('should have a max of 100 for durability')
+    // SUCCEED TEST
     describe("Your weapon has been enhanced", () => {
         it("Weapon has reached max enhancement of 20", () => {
             expect(succeed(weapon)).toEqual({ ...weapon, enhancement: weapon.enhancement + 1 });
         })
         it("Weapon has already been enhanced to the max of 20", () => {
             expect(succeed({ ...weapon, enhancement: 20 })).toEqual({ ...weapon, enhancement: 20 })
+        })
+    })
+
+    // FAIL TEST
+    describe("Your weapon failed to repair", () => {
+        it("Weapon has reached max enhancement of 20", () => {
+            expect(fail(weapon)).toEqual({ ...weapon, durability: weapon.durability - 4 });
         })
     })
 })
